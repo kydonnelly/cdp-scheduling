345d90773c60c2450f627c951119df73499af4d4
Kyle Donnelly <kydonnelly@gmail.com>
2022-03-24 22:01:59 -0700

    add locations table

--- schedule.sql
+++ schedule.sql
@@ -10,6 +10,18 @@ CREATE TABLE `shifts` (
   `validated_signatures` int(11),
   `notes` text,
   PRIMARY KEY (`shift_id`)
-)
+);
 
-INSERT INTO `wp_shifts_2022`(`shift_id`, `gatherer`, `location`, `start_time`, `end_time`, `is_bottomliner`, `capacity`, `raw_signatures`, `validated_signatures`, `notes`) VALUES (1,'test_name','test_location','2022-04-10 18:30:00','2022-04-10 20:30:00',true,2,NULL,NULL,NULL);
+INSERT INTO `shifts`(`shift_id`, `gatherer`, `location`, `start_time`, `end_time`, `is_bottomliner`, `capacity`, `raw_signatures`, `validated_signatures`, `notes`) VALUES (1,'test_name','test_location','2022-04-10 18:30:00','2022-04-10 20:30:00',true,2,NULL,NULL,NULL);
+
+CREATE TABLE `locations` (
+  `location_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `type` text NOT NULL,
+  `quality` int(11),
+  `capacity` int(11),
+  `notes` text,
+  PRIMARY KEY (`location_id`)
+);
+
+INSERT INTO `locations`(`location_id`, `name`, `type`, `quality`, `capacity`, `notes`) VALUES (1,"Local Grocery","Grocery Store",3,2"Busy location");
