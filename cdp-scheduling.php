405b09705853b621f06906f64fc7aa1b5ce1e0e5
Kyle Donnelly <kydonnelly@gmail.com>
2022-03-24 21:06:26 -0700

    Show existing shifts in table cell rows

--- cdp-scheduling.php
+++ cdp-scheduling.php
@@ -22,69 +22,95 @@ function cdp_scheduling_code() {
   $future = date_format(date_add(cdp_strtotime($today), $future_period), 'Y-m-d');
   $schedule = cdp_get_query_results(SCHEDULING_COLUMNS, "WHERE end_time > '$today' AND end_time < '$future' ORDER BY start_time ASC");
 
-  $daily_shifts = cdp_partition_daily_shifts($today, $schedule);
+  $daily_schedule = cdp_partition_daily_schedule($today, $schedule);
   for ($i = 0; $i < $days_to_show; $i++) {
-    if (!array_key_exists($i, $daily_shifts)) {
-      $daily_shifts[$i] = array();
+    if (!array_key_exists($i, $daily_schedule)) {
+      $daily_schedule[$i] = array();
     }
   }
 
-  cdp_echo_schedule_html($today, $daily_shifts);
+  cdp_echo_schedule_html($today, $daily_schedule, true);
 }
 
 function cdp_shift_reports_code() {
   $today = cdp_nowtostr();
   $schedule = cdp_get_query_results(SCHEDULING_COLUMNS, "WHERE $end_time <= '$today' ORDER BY end_time DESC");
-  $daily_shifts = cdp_partition_daily_shifts($today, $schedule);
+  $daily_schedule = cdp_partition_daily_schedule($today, $schedule);
 
-  cdp_echo_schedule_html($today, $daily_shifts);
+  cdp_echo_schedule_html($today, $daily_schedule, false);
 }
 
-function cdp_partition_daily_shifts($today, $schedule) {
+function cdp_partition_daily_schedule($today, $schedule) {
   $current_day = cdp_strtotime($today);
-  $daily_shifts = array();
+  $daily_schedule = array();
 
   // partition all the shift results into days
   foreach ($schedule as $shift_result) {
     $start_time = cdp_strtotime($shift_result->start_time);
-    $interval = date_diff($start_time, $current_day);
-    $days_apart = $interval->format('%R%a');
+    $interval = date_diff($current_day, $start_time);
+    $days_apart = intval($interval->format('%d'));
 
-    if (array_key_exists($days_apart, $daily_shifts)) {
-      $daily_shifts[$days_apart] []= $shift_result;
+    if (array_key_exists($days_apart, $daily_schedule)) {
+      $daily_schedule[$days_apart] []= $shift_result;
     } else {
-      $daily_shifts[$days_apart] = [$shift_result];
+      $daily_schedule[$days_apart] = [$shift_result];
     }
   }
 
-  return $daily_shifts;
+  return $daily_schedule;
 }
 
-function cdp_echo_schedule_html($today, $daily_shifts) {
+function cdp_echo_schedule_html($today, $daily_schedule, $is_future) {
   $current_day = cdp_strtotime($today);
+  $table_id = $is_future ? 'schedule_table' : 'reports_table';
 
-  echo '<table id="signup_table" style="width:100%" cellspacing="2" cellpadding="4">';
+  echo '<table id="' . $table_id . '" style="width:100%" cellspacing="2" cellpadding="4">';
   echo '<tbody>';
-  foreach ($daily_shifts as $day_offset => $daily_shift) {
+  foreach ($daily_schedule as $day_offset => $daily_shifts) {
     $period_offset = 'P' . $day_offset . 'D';
     $date_offset = new DateInterval($period_offset);
     $shift_day = date_add($current_day, $date_offset);
     $display_day = date_format($shift_day, 'l m/d/Y');
     date_sub($current_day, $date_offset); // need to undo the above line
-    
+
     echo '<tr>';
 
     // Date header
     echo '<th id="row_' . $period_offset . '" class="row_header" data-row-index="' . $day_offset . '" scope="row">' . $display_day . '</th>';
 
     // New shift cell
-    echo '<td class="create-shift" data-col-index="0" data-row-index="' . $day_offset . '">';
-    echo '</td>';
+    if ($is_future) {
+      echo '<td class="create-shift" data-col-index="0" data-row-index="' . $day_offset . '">';
+      echo '<ul class="shift-create">';
+      echo '<li class="create-button"><button class="create" name="create_' . $day_offset . '" onclick="createShift(this)">Create</button></li>';
+      echo '</ul>';
+      echo '</td>';
+    }
+
+    // Existing shifts
+    foreach ($daily_shifts as $shift_index => $daily_shift) {
+      $start_time = date_format(date_create($daily_shift->start_time), 'h:i A');
+      $end_time = date_format(date_create($daily_shift->end_time), 'h:i A');
+
+      echo '<td class="upcoming-shift" data-col-index="' . ($shift_index + 1) . '" data-row-index="' . $day_offset . '">';
+      echo '<ul class="shift-info">';
+      echo '<li class="shift-gatherer"><span class="name">' . $daily_shift->gatherer . '</span></li>';
+      echo '<li class="shift-location"><span class="name">' . $daily_shift->location . '</span></li>';
+      echo '<li class="shift-timestamp"><span class="name">' . $start_time . ' - ' . $end_time . '</span></li>';
+      echo '</ul>';
+
+      // Join button
+      if (intval($daily_shift->capacity) > 1) {
+        echo '<ul class="shift-join">';
+        echo '<li class="join-button"><button class="join" name="join_' . $daily_shift->shift_id . '" onclick="joinShift(this)">Join</button></li>';
+        echo '</ul>';
+      }
+      echo '</td>';
+    }
     echo '</tr>';
   }
   echo '</tbody>';
   echo '</table>';
-  echo '<button name="invalid" onclick="invalidateSearch(this)">Mark Invalid</button>';
 }
 
 function cdp_create_shift_code() {
