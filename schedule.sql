828457cf0125f6b4e2e85f1e102076e59614ae87
Kyle Donnelly <kydonnelly@gmail.com>
2022-03-26 23:24:30 -0700

    add contact field to sql schema

--- schedule.sql
+++ schedule.sql
@@ -2,6 +2,7 @@ CREATE TABLE `shifts` (
   `shift_id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11),
   `gatherer` text NOT NULL,
+  `contact` text NOT NULL,
   `location_id` int(11) NOT NULL,
   `start_time` timestamp NOT NULL,
   `end_time` timestamp NOT NULL,
@@ -14,7 +15,7 @@ CREATE TABLE `shifts` (
   FOREIGN KEY (`location_id`) REFERENCES locations(`location_id`)
 );
 
-INSERT INTO `shifts`(`shift_id`, `parent_id`, `gatherer`, `location_id`, `start_time`, `end_time`, `capacity`, `cancelled`, `raw_signatures`, `validated_signatures`, `notes`) VALUES (1,NULL,'test_name',1,'2022-04-10 18:30:00','2022-04-10 20:30:00',2,false,NULL,NULL,NULL);
+INSERT INTO `shifts`(`shift_id`, `parent_id`, `gatherer`, `contact`, `location_id`, `start_time`, `end_time`, `capacity`, `cancelled`, `raw_signatures`, `validated_signatures`, `notes`) VALUES (1,NULL,'test_name',`test_phone',1,'2022-04-10 18:30:00','2022-04-10 20:30:00',2,false,NULL,NULL,NULL);
 
 CREATE TABLE `locations` (
   `location_id` int(11) NOT NULL AUTO_INCREMENT,
